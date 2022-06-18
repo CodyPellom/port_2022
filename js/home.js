e48c8c68cb9bec734a78804c27b45d6e4efc5021
CodyPellom <paulpellom@gmail.com>
2022-06-18 12:59:44 -0400

    slideshow w/ text

--- js/home.js
+++ js/home.js
@@ -5,6 +5,21 @@ let introHeader = document.getElementById('intro-header');
 let introAnimatedText = document.getElementById('intro-header-animated-text');
 let introDescrip = document.getElementById('intro-descrip-text');
 let introImgSlider = document.getElementById('intro-image-changer');
+let introImg1 = document.getElementById('introImg1');
+let introImg2 = document.getElementById('introImg2');
+let introImg3 = document.getElementById('introImg3');
+let introImg4 = document.getElementById('introImg4');
+let introImg5 = document.getElementById('introImg5');
+let introImg6 = document.getElementById('introImg6');
+let introImgText = document.getElementById('introImgTxt');
+
+setInterval(function(){
+    navbarHeader.classList = "major-mono";
+}, 1500);
+
+setInterval(function(){
+    navbarHeader.classList = "major-mono neon";
+}, 1100);
 
 /*timeline example
 var introAnimation = anime.timeline({
@@ -22,27 +37,14 @@ var introAnimation = anime.timeline({
   .add({
     backgroundColor: "#9bffda",
   })
-  .add({
-    backgroundColor: "#89ffd3",
-  })
-  .add({
-    backgroundColor: "#75ffcc",
-  })
-  .add({
-    backgroundColor: "#89ffd3",
-  })
-  .add({
-    backgroundColor: "#9bffda",
-  })
-  .add({
-    backgroundColor: "#adffe1",
-  })
-  */
+*/
 var typewriter = new Typewriter(introHeader, {
     //loop: true
 });
 
 typewriter.typeString('whoami').start();
+
+var introTyper = new Typewriter(introImgText, {});
 
 // let type2 = new Typewriter(introDescrip, {});
 // type2.typeString(`my name is cody pellom. i am a professional web developer and software engineer who specializes in 
@@ -62,3 +64,90 @@ typewriter.typeString('whoami').start();
     // .pauseFor(2500)
     // .deleteChars(40)
     // .start();
+
+    function imgSlideOut(img) {
+        anime({
+            targets: img,
+            easing: 'easeOutExpo',
+            duration: 500,
+            opacity: 0,
+            display: 'none'
+        })
+    }
+    function imgSlideIn(img) {
+        anime({
+            targets: img,
+            easing: 'easeOutExpo',
+            opacity: 0,
+            opacity: 1,
+            duration: 500,
+            //display: 'unset'
+        })
+    }
+    let imgCount = introImgSlider.childElementCount;
+    let initSlideCount = 0; 
+
+    setInterval(function(){
+        initSlideCount++;
+        if (initSlideCount === 6) {
+            initSlideCount = 0;
+        }
+        if (initSlideCount === 0) {
+            introImg1.style.opacity = '1';
+            introImg2.style.opacity = '0';
+            introImg3.style.opacity = '0';
+            introImg4.style.opacity = '0';
+            introImg5.style.opacity = '0';
+            introImg6.style.opacity = '0';
+            introTyper.deleteAll();
+            introTyper.typeString('A Professional').start();
+        }
+        if (initSlideCount === 1) {
+            introImg1.style.opacity = '0';
+            introImg2.style.opacity = '1';
+            introImg3.style.opacity = '0';
+            introImg4.style.opacity = '0';
+            introImg5.style.opacity = '0';
+            introImg6.style.opacity = '0';
+            introTyper.deleteAll();
+            introTyper.typeString('A Family Man').start();
+        }
+        if (initSlideCount === 2) {
+            introImg1.style.opacity = '0';
+            introImg2.style.opacity = '0';
+            introImg3.style.opacity = '1';
+            introImg4.style.opacity = '0';
+            introImg5.style.opacity = '0';
+            introImg6.style.opacity = '0';
+            introTyper.deleteAll();
+            introTyper.typeString('A Hopeless Romantic').start();
+        }
+        if (initSlideCount === 3) {
+            introImg1.style.opacity = '0';
+            introImg2.style.opacity = '0';
+            introImg3.style.opacity = '0';
+            introImg4.style.opacity = '1';
+            introImg5.style.opacity = '0';
+            introImg6.style.opacity = '0';
+        }
+        if (initSlideCount === 4) {
+            introImg1.style.opacity = '0';
+            introImg2.style.opacity = '0';
+            introImg3.style.opacity = '0';
+            introImg4.style.opacity = '0';
+            introImg5.style.opacity = '1';
+            introImg6.style.opacity = '0';
+            introTyper.deleteAll();
+            introTyper.typeString('Sometimes Spooky ...').start();
+        }
+        if (initSlideCount === 5) {
+            introImg1.style.opacity = '0';
+            introImg2.style.opacity = '0';
+            introImg3.style.opacity = '0';
+            introImg4.style.opacity = '0';
+            introImg5.style.opacity = '0';
+            introImg6.style.opacity = '1';
+            introTyper.deleteAll();
+            introTyper.typeString('Sometimes Goofy').start();
+        }
+    }, 6500);
